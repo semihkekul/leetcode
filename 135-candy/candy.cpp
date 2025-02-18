551d000527e844f3372103b8521f071ba2d1275e
Semih Kekül <semihkekul@users.noreply.github.com>
2025-02-18 14:52:21 +0100

    Time: 0 ms (100.00%) | Memory: 23 MB (26.31%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> candies(ratings.size(), 1);
+
+        for(int i = 0; i < ratings.size() - 1; ++i)
+        {
+            if(ratings[i+1] > ratings[i] && candies[i+1] <= candies[i] )
+            {
+                candies[i+1] = candies[i] + 1;
+            }
+        }
+
+        for(int i = ratings.size() - 1; i > 0; --i)
+        {
+            if(ratings[i-1] > ratings[i] && candies[i-1] <= candies[i] )
+            {
+                candies[i-1] = candies[i] + 1;
+            }
+        }
+
+        return std::accumulate(candies.begin(), candies.end(), 0);
+    }
+};
+
+/*
+1 2 3 2 1
+1 2 3 1 
+*/
